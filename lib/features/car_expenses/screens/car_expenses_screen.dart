db10dcbb30176a5b8eb159f93e73c30b38989bf8
kiryanovartem <art1718em@yandex.ru>
2025-10-25 07:29:26 +0300

    картинка в заголовке

--- lib/features/car_expenses/screens/car_expenses_screen.dart
+++ lib/features/car_expenses/screens/car_expenses_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/expense_model.dart';
 import '../widgets/expense_table.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CarExpensesScreen extends StatelessWidget {
   final List<ExpenseModel> expenses;
@@ -20,7 +21,21 @@ class CarExpensesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Расходы: ${totalAmount.toStringAsFixed(2)} руб.'),
+        centerTitle: true,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CachedNetworkImage(
+              imageUrl: "https://cdn-icons-png.flaticon.com/512/2921/2921222.png",
+              width: 30,
+              height: 30,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            const SizedBox(width: 8),
+            Text('Расходы: ${totalAmount.toStringAsFixed(2)} руб.'),
+          ],
+        ),
       ),
       body: ExpenseTable(
         expenses: expenses,
